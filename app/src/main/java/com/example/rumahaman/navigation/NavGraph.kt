abadae509dfefbec54878e6050762add394ca276
Kirukato936 <ivandiamond2004@gmail.com>
2025-10-29 23:57:12 +0700

    Menambahkan halaman OTP (belum final karena belum bisa direview dikarenakan halaman lupa passwordnya belum ada)

--- app/src/main/java/com/example/rumahaman/navigation/NavGraph.kt
+++ app/src/main/java/com/example/rumahaman/navigation/NavGraph.kt
@@ -2,10 +2,15 @@
 
 package com.example.rumahaman.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.rumahaman.presentation.KodeOTP.OtpRequestScreen
+import com.example.rumahaman.presentation.KodeOTP.OtpVerifyScreen
 import com.example.rumahaman.presentation.login.LoginScreen
 import com.example.rumahaman.presentation.register.RegisterScreen
 // import com.example.rumahaman.presentation.login.LoginScreen // Anda akan butuh ini nanti
@@ -15,6 +20,8 @@ object Routes {
     const val REGISTER_SCREEN = "register" // Nama rute bisa dibuat lebih simpel
     const val LOGIN_SCREEN = "login"
     // const val HOME_SCREEN = "home_screen"
+    const val OTP_REQUEST_SCREEN = "otp_request"
+    const val OTP_VERIFY_SCREEN = "otp_verify"
 }
 
 @Composable
@@ -37,6 +44,30 @@ fun AppNavHost() {
         composable(Routes.LOGIN_SCREEN) {
 
             LoginScreen(navController = navController)
+        }
+        // ---------- OTP REQUEST ----------
+        composable(
+            route = Routes.OTP_REQUEST_SCREEN,
+            enterTransition = { slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(500)) },
+            exitTransition = { slideOutHorizontally(targetOffsetX = { -it / 2 }, animationSpec = tween(500)) }
+        ) {
+            OtpRequestScreen(
+                onGetOtpClick = {
+                    navController.navigate(Routes.OTP_VERIFY_SCREEN)
+                }
+            )
+        }
+        // ---------- OTP VERIFY ----------
+        composable(
+            route = Routes.OTP_VERIFY_SCREEN,
+            enterTransition = { slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(500)) },
+            exitTransition = { slideOutHorizontally(targetOffsetX = { -it / 2 }, animationSpec = tween(500)) }
+        ) {
+            OtpVerifyScreen(
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
