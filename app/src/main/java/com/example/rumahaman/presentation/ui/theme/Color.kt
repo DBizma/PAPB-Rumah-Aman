907544be3c45a159a421d99f05218acff2672450
Andikasaktih <andikasaktih@gmail.com>
2025-11-05 10:47:21 +0700

    add color

--- app/src/main/java/com/example/rumahaman/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/rumahaman/presentation/ui/theme/Color.kt
@@ -4,3 +4,4 @@ import androidx.compose.ui.graphics.Color
 val TealColor = Color(0xFF3B8C83)
 val LightGreenGray = Color(0xFFD9E6E2)
 val LinkColor = Color(0xFF3B8C83) // Warna yang sama dengan tombol untuk konsistensi
+val Orange = Color(0xFFF39B80)
